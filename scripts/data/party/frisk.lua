4254d9835df4d9961b6f5638ab7d93f381a5e4e7
burgertrucking <225804275+burgertrucking@users.noreply.github.com>
2025-10-22 00:59:25 -0700

    nameswap copypasta job
    man i feel like such a script kiddie

---
+++ scripts/data/party/frisk.lua
@@ -0,0 +1,224 @@
+-- This is literally copied from the kristal source code lmao
+-- BSD 3-Clause License
+
+-- Copyright (c) 2021, SylviBlossom
+-- All rights reserved.
+
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+
+-- 1. Redistributions of source code must retain the above copyright notice, this
+--    list of conditions and the following disclaimer.
+
+-- 2. Redistributions in binary form must reproduce the above copyright notice,
+--    this list of conditions and the following disclaimer in the documentation
+--    and/or other materials provided with the distribution.
+
+-- 3. Neither the name of the copyright holder nor the names of its
+--    contributors may be used to endorse or promote products derived from
+--    this software without specific prior written permission.
+
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+-- DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+-- FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+-- DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+-- SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+-- CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+-- OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+-- OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+local character, super = Class(PartyMember, "frisk")
+
+function character:init()
+    super.init(self)
+
+    -- Display name
+    self.name = "Frisk"
+
+    -- Actor (handles overworld/battle sprites)
+    self:setActor("kris")
+    self:setLightActor("kris_lw")
+    self:setDarkTransitionActor("kris_dark_transition")
+
+    -- Display level (saved to the save file)
+    self.level = Game.chapter
+    -- Default title / class (saved to the save file)
+    if Game.chapter == 1 then
+        self.title = "Leader\nCommands the party\nwith various ACTs."
+    elseif Game.chapter == 2 then
+        self.title = "Tactician\nCommands the party\nby ACTs. Sometimes."
+    elseif Game.chapter == 3 then
+        self.title = "Tactician\nCommands the party\nby ACTs."
+    else
+        self.title = "Dark Hero\nCarries out fate\nwith the blade."
+    end
+
+    -- Determines which character the soul comes from (higher number = higher priority)
+    self.soul_priority = 2
+    -- The color of this character's soul (optional, defaults to red)
+    self.soul_color = {1, 0, 0}
+
+    -- Whether the party member can act / use spells
+    self.has_act = true
+    self.has_spells = false
+
+    -- Whether the party member can use their X-Action
+    self.has_xact = true
+    -- X-Action name (displayed in this character's spell menu)
+    self.xact_name = "K-Action"
+
+    -- Current health (saved to the save file)
+    if Game.chapter == 1 then
+        self.health = 90
+    elseif Game.chapter == 2 then
+        self.health = 120
+    elseif Game.chapter == 3 then
+        self.health = 160
+    else
+        self.health = 200
+    end
+
+    -- Base stats (saved to the save file)
+    if Game.chapter == 1 then
+        self.stats = {
+            health = 90,
+            attack = 10,
+            defense = 2,
+            magic = 0
+        }
+    elseif Game.chapter == 2 then
+        self.stats = {
+            health = 120,
+            attack = 12,
+            defense = 2,
+            magic = 0
+        }
+    elseif Game.chapter == 3 then
+        self.stats = {
+            health = 160,
+            attack = 14,
+            defense = 2,
+            magic = 0
+        }
+    else
+        self.stats = {
+            health = 200,
+            attack = 17,
+            defense = 2,
+            magic = 0
+        }
+
+    end
+    -- Max stats from level-ups
+    if Game.chapter == 1 then
+        self.max_stats = {
+            health = 120
+        }
+    elseif Game.chapter == 2 then
+        self.max_stats = {
+            health = 160
+        }
+    elseif Game.chapter == 3 then
+        self.max_stats = {
+            health = 200
+        }
+    else
+        self.max_stats = {
+            health = 240
+        }
+    end
+    
+    -- Party members which will also get stronger when this character gets stronger, even if they're not in the party
+    self.stronger_absent = {"frisk","suzy","ralsei"}
+
+    -- Weapon icon in equip menu
+    self.weapon_icon = "ui/menu/equip/sword"
+
+    -- Equipment (saved to the save file)
+    self:setWeapon("wood_blade")
+    if Game.chapter >= 2 then
+        self:setArmor(1, "amber_card")
+        self:setArmor(2, "amber_card")
+    end
+
+    -- Default light world equipment item IDs (saves current equipment)
+    self.lw_weapon_default = "light/pencil"
+    self.lw_armor_default = "light/bandage"
+
+    -- Character color (for action box outline and hp bar)
+    self.color = {0, 1, 1}
+    -- Damage color (for the number when attacking enemies) (defaults to the main color)
+    self.dmg_color = {0.5, 1, 1}
+    -- Attack bar color (for the target bar used in attack mode) (defaults to the main color)
+    self.attack_bar_color = {0, 162/255, 232/255}
+    -- Attack box color (for the attack area in attack mode) (defaults to darkened main color)
+    self.attack_box_color = {0, 0, 1}
+    -- X-Action color (for the color of X-Action menu items) (defaults to the main color)
+    self.xact_color = {0.5, 1, 1}
+
+    -- Head icon in the equip / power menu
+    self.menu_icon = "party/kris/head"
+    -- Path to head icons used in battle
+    self.head_icons = "party/kris/icon"
+    -- Name sprite
+    self.name_sprite = "party/frisk/name"
+
+    -- Effect shown above enemy after attacking it
+    self.attack_sprite = "effects/attack/cut"
+    -- Sound played when this character attacks
+    self.attack_sound = "laz_c"
+    -- Pitch of the attack sound
+    self.attack_pitch = 1
+
+    -- Battle position offset (optional)
+    self.battle_offset = {2, 1}
+    -- Head icon position offset (optional)
+    self.head_icon_offset = nil
+    -- Menu icon position offset (optional)
+    self.menu_icon_offset = nil
+
+    -- Message shown on gameover (optional)
+    self.gameover_message = nil
+end
+
+function character:onLevelUp(level)
+    self:increaseStat("health", 2)
+    if level % 10 == 0 then
+        self:increaseStat("attack", 1)
+    end
+end
+
+function character:onPowerSelect(menu)
+    if MathUtils.random() < ((Game.chapter == 1) and 0.02 or 0.04) then
+        menu.kris_dog = true
+    else
+        menu.kris_dog = false
+    end
+end
+
+function character:drawPowerStat(index, x, y, menu)
+    if index == 1 and menu.kris_dog then
+        local frames = Assets.getFrames("misc/dog_sleep")
+        local frame = math.floor(Kristal.getTime()) % #frames + 1
+        love.graphics.print("Dog:", x, y)
+        Draw.draw(frames[frame], x+120, y+5, 0, 2, 2)
+        return true
+    elseif index == 3 then
+        local icon = Assets.getTexture("ui/menu/icon/fire")
+        Draw.draw(icon, x-26, y+6, 0, 2, 2)
+        love.graphics.print("Guts:", x, y)
+
+        Draw.draw(icon, x+90, y+6, 0, 2, 2)
+        if Game.chapter >= 2 then
+            Draw.draw(icon, x+110, y+6, 0, 2, 2)
+        end
+        if Game.chapter >= 4 then
+            Draw.draw(icon, x+130, y+6, 0, 2, 2)
+        end
+        return true
+    end
+end
+
+return character
